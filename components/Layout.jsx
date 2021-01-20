3ba882fef844e97531b2e51320cacdf6d0e27777
Jhon Angulo <jhonmanuelm89@gmail.com>
2021-01-19 22:30:44 -0500

    add userinfo component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,12 @@
 import Header from './Header'
 import Footer from  './Footer'
+import UserInfo from './UserInfo'
 
 function Layout({ children }) {
   return (
     <>
     <Header />
+    <UserInfo />
     {
       children
     }
---
+++ components/UserInfo.jsx
@@ -0,0 +1,14 @@
+
+function UserInfo() {
+  return (
+    <div className='user__info'>
+      <a href="" >
+        <img className='user__avatar' src="https://react-material-dashboard.devias.io/static/images/avatars/avatar_7.png" alt=""/> 
+      </a>
+        <h5 className='user__name'>Jhon Angulo</h5>
+        <p className='user__position'>Desarrollador</p>
+    </div>
+  )
+}
+
+export default UserInfo
