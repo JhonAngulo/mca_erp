36924ec03b905abf8178fc3a6564bb3eb683a141
Jhon Angulo <jhonmanuelm89@gmail.com>
2021-01-19 22:13:47 -0500

    created layout

---
+++ components/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <header className='header'>
+      <a href="" className='header__logo'>
+        MCA - ERP
+      </a>
+      <nav className='header__nav'>
+        <ul className='header__nav-list'>
+          <li className='header__nav-item'>Configuración</li>
+          <li className='header__nav-item'>Salir</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
